2e3e94a917d758163cd9564c06cecb2d2f52709d
Behnood Abdollahi <behnood.abdollahi2011@gmail.com>
2026-07-01 12:55:13 +0330

    Implement the cursor type

---
+++ crates/oxide-core/src/cursor.rs
@@ -0,0 +1,35 @@
+use oxide_buffer::Position;
+
+pub struct Cursor {
+    position: Position,
+}
+
+impl Cursor {
+    pub fn new() -> Self {
+        Self {
+            position: Position::new(0, 0),
+        }
+    }
+
+    pub fn position(&self) -> Position {
+        self.position
+    }
+
+    pub fn set(&mut self, pos: Position) {
+        self.position = pos;
+    }
+
+    pub fn line(&self) -> usize {
+        self.position.line
+    }
+
+    pub fn column(&self) -> usize {
+        self.position.column
+    }
+}
+
+impl Default for Cursor {
+    fn default() -> Self {
+        Self::new()
+    }
+}
